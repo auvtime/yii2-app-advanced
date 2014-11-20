b98f666aa997efbf5cf6af0ad89e07659d3c7f14
WangXianfeng <wangxianfeng@auvtime.com>
2014-11-20 20:22:56 +0800

    添加百度统计代码

--- frontend/auvtime/headroom/js/auvtimeHead.js
+++ frontend/auvtime/headroom/js/auvtimeHead.js
@@ -10,4 +10,6 @@ $(document).ready(function() {
 			"notTop" : "headroom--not-top"
 		}
 	});
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fdfb059dd6dbf96abae9e238dce5aa190' type='text/javascript'%3E%3C/script%3E"));
 });
